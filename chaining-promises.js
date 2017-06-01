81034ce093787b32dcb56b32efcd6623d3ac71b4
Mahtab Alam <alammahtab08@gmail.com>
2017-06-01 08:35:18 +0530

    Added error handling

--- chaining-promises.js
+++ chaining-promises.js
@@ -1,7 +1,7 @@
 var axios=require('axios');
 var URL="http://localhost:3000";
 
-axios.get(URL+'/olympic/2016/ranking/4')
+axios.get(URL+'/olympic/2016/ranking/42')
      .then(function(response){     	
      	return axios.get(URL+'/iso/country/'+response.data.Country);
      })
@@ -12,7 +12,7 @@ axios.get(URL+'/olympic/2016/ranking/4')
      	console.log(response.data)
      })
      .catch(function(error){
-        console.log(error);
+          console.log(error)
      });
 
 
